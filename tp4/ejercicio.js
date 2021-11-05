bdc0b07923415a3f1b37554c401ad853f9f9dfc5
Pedro Mattenet Riva <pmattenet@gmail.com>
2021-11-04 22:30:03 -0300

    remove extra code

--- tp4/ejercicio.js
+++ tp4/ejercicio.js
@@ -244,39 +244,3 @@ var meshFS = `
 		gl_FragColor = tEnabled * texture2D(uSampler, var_text_coord) + (1.0 - tEnabled) * vec4(1.0, 0.0, 0.0, 1.0);
 	}
 `;
-
-//
-// Initialize a texture and load an image.
-// When the image finished loading copy it into the texture.
-//
-function loadTexture(image) {
-	const texture = gl.createTexture();
-
-	const level = 0;
-	const internalFormat = gl.RGBA;
-	const srcFormat = gl.RGBA;
-	const srcType = gl.UNSIGNED_BYTE;
-	
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-				srcFormat, srcType, image);
-
-	// WebGL1 has different requirements for power of 2 images
-	// vs non power of 2 images so check if the image is a
-	// power of 2 in both dimensions.
-	if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-		// Yes, it's a power of 2. Generate mips.
-		gl.generateMipmap(gl.TEXTURE_2D);
-	} else {
-		// No, it's not a power of 2. Turn off mips and set
-		// wrapping to clamp to edge
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	}
-	return texture;
-}
-  
-function isPowerOf2(value) {
-	return (value & (value - 1)) == 0;
-}
